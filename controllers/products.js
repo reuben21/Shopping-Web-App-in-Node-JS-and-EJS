d949c3bc63d9f264957cd9d6e766dc13894882cc
reuben21 <reuben211999@icloud.com>
2020-12-18 17:07:43 +0530

    Updated with File Read and Write (JSON)

--- controllers/products.js
+++ controllers/products.js
@@ -18,12 +18,26 @@ exports.postAddProduct = (req, res, next) => {
     res.redirect(`/`);
 }
 
-exports.getProducts = (req, res, next) => {
-    const products = Product.fetchAll();
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
+// exports.getProducts = (req, res, next) => {
+//     const products = Product.fetchAll();
+//     res.render('shop', {
+//         prods: products,
+//         pageTitle: 'Shop',
+//         path: '/',
 
-      });
+//       });
+// }
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll(
+        products => {
+            res.render('shop', {
+                prods: products,
+                pageTitle: 'Shop',
+                path: '/',
+        
+              });
+        }
+    );
+    
 }
--- models/product.js
+++ models/product.js
@@ -1,6 +1,9 @@
 const products = [];
-
-
+const fs = require('fs');
+const path = require('path');
+const p = path.join(path.dirname(process.mainModule.filename),
+'data','products.json'
+)
 module.exports = class Product {
     // t - title
     constructor(t, p) {
@@ -9,14 +12,40 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(
-            {
-                title: this.title,
-                price: this.price
-            });
+       
+        fs.readFile(p,(err,fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(
+                {
+                    title: this.title,
+                    price: this.price
+                });
+            
+            
+            fs.writeFile(p,JSON.stringify(products),err => {
+                console.log(err)
+            })
+           
+            
+        })
+        // products.push(
+        //     {
+        //         title: this.title,
+        //         price: this.price
+        //     });
+        
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        fs.readFile(p,(err,fileContent) => {
+            if (err) { 
+                cb([]); 
+            }
+           cb(JSON.parse(fileContent));
+        })
+        // return products;
     }
 }
