d4f0a5bc99495a14d7b59f396c68af21f4401a28
reuben21 <reuben211999@icloud.com>
2021-01-09 22:12:36 +0530

    CRUD with Mongoose

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const expressHbs = require('express-handlebars');
-
+const mongoose = require('mongoose');
 const app = express();
 
 
@@ -15,20 +15,20 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error')
 
-const User = require('./models/user');
+// const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')))
 
-app.use((req, res,next)=>{
-    User.findById("5fe4a8e2c409135c1839c35a")
-    .then(user => {
-        req.user =new User (user.username,user.email,user.password,user.cart,user._id);
-        next();
-    }).catch(err => {
-        console.log(err);
-    });
-})
+// app.use((req, res,next)=>{
+//     User.findById("5fe4a8e2c409135c1839c35a")
+//     .then(user => {
+//         req.user =new User (user.username,user.email,user.password,user.cart,user._id);
+//         next();
+//     }).catch(err => {
+//         console.log(err);
+//     });
+// })
 
 app.use('/admin',adminRoutes);
 
@@ -42,8 +42,13 @@ app.use(errorController.get404)
 //     });
 // };
 
-mongoConnect(()=>{
-    
-    app.listen(3000);
-})
+mongoose.connect('mongodb+srv://user_for_node:7uFBJ7025U5qD5Av@mongodb.syifj.mongodb.net/Shopping_Store?retryWrites=true&w=majority')
+    .then(res =>{
+        app.listen(3000);
+    }).catch(err =>{
+    console.log(err);
+});
+
+
+
 
